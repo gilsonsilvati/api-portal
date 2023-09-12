03209eee517ec2b1b18c0d3b522f3bdf63389a39
Gilson Silva <ext.gilsoncs@unicid.edu.br>
2023-09-12 14:33:55 -0300

    Adding logs

--- src/main/java/com/portal/api/message/KafkaProducerMessage.java
+++ src/main/java/com/portal/api/message/KafkaProducerMessage.java
@@ -1,6 +1,8 @@
 package com.portal.api.message;
 
 import com.portal.api.dto.CarDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -8,12 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaProducerMessage {
 
-    @Autowired
-    protected KafkaTemplate<String, CarDTO> kafkaTemplate;
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaProducerMessage.class);
 
     private static final String KAFKA_TOPIC = "car-topic";
 
+    @Autowired
+    protected KafkaTemplate<String, CarDTO> kafkaTemplate;
+
     public void send(CarDTO carDTO) {
+        LOG.info("API PORTAL - Producer Car Post information: {}", carDTO);
 
         kafkaTemplate.send(KAFKA_TOPIC, carDTO);
     }
